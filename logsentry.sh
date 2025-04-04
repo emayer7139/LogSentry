435dc3ca9c540f2a419764bd2552bb0bb051023d
emayer7139 <135661341+emayer7139@users.noreply.github.com>
2025-04-04 11:53:29 -0400

    Update logsentry.sh

--- logsentry.sh
+++ logsentry.sh
@@ -5,16 +5,15 @@ source "$CONFIG"
 
 declare -A ATTEMPTS
 
-echo "[*] LogSentry started. Watching: $LOG_FILE"
-
 tail -F "$LOG_FILE" | while read -r line; do
     if [[ "$line" =~ "Failed password for" ]]; then
-        IP=$(echo "$line" | grep -oP 'from \K[\d\.]+')
+        IP=$(echo "$line" | grep -oP 'from \K\S++')
         TIMESTAMP=$(date +%s)
 
         if [[ -n "$IP" ]]; then
             ATTEMPTS["$IP,$TIMESTAMP"]=1
 
+            # Count attempts in the last $TIMEFRAME seconds
             COUNT=0
             NOW=$(date +%s)
             for key in "${!ATTEMPTS[@]}"; do
@@ -31,7 +30,6 @@ tail -F "$LOG_FILE" | while read -r line; do
                     sudo ufw deny from "$IP"
                     echo "[+] IP $IP blocked using ufw"
                 fi
-                sleep 2  # prevent spam on same detection loop
             fi
         fi
     fi
